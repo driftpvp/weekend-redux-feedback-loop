7c9258a0a247a0d70f781b7c039c20afd3db9c48
Lucas Martin <driftpvp@gmail.com>
2023-09-20 15:52:48 -0500

    folder structure, reducer, and store set up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//Components
 import './index.css';
 import App from './components/App/App';
 
+// Redux
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+
+//reducer
+const scaleCount = (state = 0 , action) => {
+    console.log('reducer is go');
+    return state;
+};
+
+//store
+const storeInstance = createStore (
+    scaleCount
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <React.StrictMode store={storeInstance}>
         <App />
     </React.StrictMode>
 );
